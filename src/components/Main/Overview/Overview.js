8ed40052032f30d7e34a5c8cdc168f752d633a5b
Mikhail Surikov <miketogo@yandex.ru>
2021-11-13 13:36:09 +0300

    fix: meta + pdf wp

--- src/components/Main/Overview/Overview.js
+++ src/components/Main/Overview/Overview.js
@@ -5,6 +5,7 @@ import './Overview.css';
 
 import diamond from '../../../assets/images/overview/diamond.png'
 import coins from '../../../assets/images/overview/coins.png'
+import wp from '../../../assets/files/BDR_WhitePaper.pdf'
 
 import { Link } from "react-router-dom";
 
@@ -25,9 +26,9 @@ function Overview() {
         <p className="overview__intro-text">BDR provides asset liquidity for the real dimond market by producing and tokenizing man-made dimonds. All produced diamonds are equal to each other enabling us to make each coin equal in its price.</p>
         <div className="overview__intro-links">
           <Link to="/technology" className="overview__intro-link">Read about our technology</Link>
-          <div className="overview__intro-button">
+          <a target="_blank" rel="noreferrer" href={wp} className="overview__intro-button">
             <p className="overview__intro-button-text">Read whitepaper</p>
-          </div>
+          </a>
         </div>
       </div>
       <div className="overview__coin-cap">
